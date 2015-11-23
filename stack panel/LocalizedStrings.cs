bfdd26fdbb251b3a4808c7fd074b07ec5a04b3c0
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-11-23 22:53:36 +0000

    init

---
+++ stack panel/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Stack_Panel.Resources;
+
+namespace Stack_Panel
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
